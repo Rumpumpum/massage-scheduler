2c5c1292b7035b2eb8cf49d1c57a2fbdf6380ec6
unknown <darginchik11@gmail.com>
2025-02-22 09:47:43 +0300

    Added my Telegram ID to auth.js

--- auth.js
+++ auth.js
@@ -5,13 +5,13 @@ export function initializeAuth() {
     const userId = initData?.user?.id;
 
     const masseurs = {
-        "952232290": "Анна",  // Замените на реальные Telegram ID
-        //"987654321": "Игорь",
-        //"456789123": "Мария"
+        "952232290": "Анна",    // Замените "ВАШ_ID" на ваш Telegram ID (число, например, "123456789")
+        "987654321": "Игорь",
+        "456789123": "Мария"
     };
 
     const currentMasseur = masseurs[userId] || null;
-    if (!currentMasseur) {
+    if (!952232290) {
         window.Telegram.WebApp.showAlert("У вас нет доступа к управлению записями.");
     }
 
